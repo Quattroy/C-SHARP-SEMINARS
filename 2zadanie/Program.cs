75397fc84ebae7a9840d9ecf0f7f3cf466287a2b
Igor Isupov <igor-i.s1@ya.ru>
2023-02-23 22:52:51 +0300

    добавлено 2 задание

---
+++ 2zadanie/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write ("Введите номер дня недели от 1 до 7: "); 
+int userNumber1 = int.Parse(Console.ReadLine() ?? ""); 
+if (userNumber1 == 1)
+    Console.Write ("Понедельник");
+else if (userNumber1 == 2)
+    Console.Write ("Вторник");
+else if (userNumber1 == 3)
+    Console.Write ("Среда");
+else if (userNumber1 == 4)
+    Console.Write ("Четверг");
+else if (userNumber1 == 5)
+    Console.Write ("Пятница");
+else if (userNumber1 == 6)
+    Console.Write ("Суббота");
+else if (userNumber1 == 7)
+    Console.Write ("Воскресенье");
+else    
+    Console.Write ("Вы ввели некорректное число!"); 
+    
