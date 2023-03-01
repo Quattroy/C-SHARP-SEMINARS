411c1db845caab008137a5abf55e05f12dd02f10
Igor Isupov <igor-i.s1@ya.ru>
2023-03-01 21:36:54 +0300

    Задание №12

---
+++ 12.kratnost/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число");
+double firstNumber = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+double secondNumber = double.Parse(Console.ReadLine());
+
+if(secondNumber % firstNumber ==0)
+{
+    Console.WriteLine("Второе число является кратным первому");
+}
+else
+{
+    Console.WriteLine(secondNumber % firstNumber);
+}
