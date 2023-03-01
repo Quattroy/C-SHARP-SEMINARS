27b7d0149d7fc6ce79fab758c4e1600bb9da1080
Igor Isupov <igor-i.s1@ya.ru>
2023-03-01 22:18:41 +0300

    задание 14

--- 12.kratnost/Program.cs
+++ 12.kratnost/Program.cs
@@ -4,7 +4,7 @@ double firstNumber = double.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 double secondNumber = double.Parse(Console.ReadLine());
 
-if(firstNumber % secondNumber ==0)
+if (firstNumber % secondNumber == 0)
 {
     Console.WriteLine($"{firstNumber}, {secondNumber} -> кратно");
 }
---
+++ 14.kratno 7&&23/Program.cs
@@ -0,0 +1,15 @@
+﻿//14. Напишите программу, которая принимает на вход число и проверяет, 
+//крато ли оно одновременно 7 и 23
+
+Console.WriteLine("Введите число");
+double Number = double.Parse(Console.ReadLine());
+
+
+if (Number % 7 == 0 && Number % 23 == 0)
+{
+    Console.WriteLine($"{Number} -> кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine($"{Number} -> не кратно, 7 и 23");
+}
