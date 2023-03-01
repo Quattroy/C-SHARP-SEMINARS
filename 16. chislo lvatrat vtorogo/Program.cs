0351a5cb2560f6d31d7d09ac824f9cb1908297c0
Igor Isupov <igor-i.s1@ya.ru>
2023-03-01 23:12:53 +0300

    задание 16

---
+++ 16. chislo lvatrat vtorogo/Program.cs
@@ -0,0 +1,19 @@
+﻿// 16. Напишите программу, которая принимает на вход 2 числа и проверяет, 
+//является ли одно число квадратом другого
+
+Console.Write("Введите первое целое число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+bool result = (number1 == number2 * number2) || (number2 == number1 * number1);
+
+if (result)
+{
+Console.WriteLine($"{number1}, {number2} -> ДА");
+}
+else
+{
+Console.WriteLine($"{number1}, {number2} -> НЕТ");
+}
