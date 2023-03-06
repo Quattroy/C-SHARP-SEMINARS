b7a1696b15ab07fceff0a4e105fbb89f46bb22b8
Igor Isupov <igor-i.s1@ya.ru>
2023-03-06 21:10:42 +0300

    задача 26

---
+++ 26/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+int num = GetNum("Введите целое число: ", "Ошибка ввода");
+int result = GetSumNumbers(num);
+PrintResult(num, result);
+
+
+
+int GetNum(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect) //&& userNumber>=0
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+// Возвращает сумму чисел от 1 до number
+int GetSumNumbers(int number)
+{
+    if(number < 0) number *= -1;
+    int count = 0;
+    if(number == 0) count = 1;
+    while (number > 0)
+    {
+        count++;
+        number /= 10; //number = number /10;
+    }
+    return count;
+}
+
+static void PrintResult(int num, int result)
+{
+Console.WriteLine($"количество цифр числа {num} равно {result}");
+}
