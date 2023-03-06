a40c0c5b29998450bb2209ae711e3c76a4acb6bf
Igor Isupov <igor-i.s1@ya.ru>
2023-03-06 22:34:58 +0300

    задача 30, массив

---
+++ 28/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 28: Напишите программу, которая принимает на 
+//вход число N и выдаёт произведение чисел от 1 до N.
+
+
+Console.Clear();
+int num = GetNumberFromUser("Введите целое число N: ", "Ошибка ввода!");
+int result = GetMultiplication(num);
+Console.WriteLine($"{num} -> {result}");
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect && userNumber>0)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int GetMultiplication (int number)
+{
+    int sum = 1;
+    while (number > 0)
+    {
+        sum *= number;
+        number--;
+    }
+    return sum;
+}
+
---
+++ 30/Program.cs
@@ -0,0 +1,28 @@
+﻿// Решение в группах задач:
+//Задача 30: Напишите программу, которая выводит массив из 8 элементов,
+// заполненный нулями и единицами в случайном порядке.
+
+int[] array = InputArray();
+PrintArray(array);
+//Console.WriteLine($"[{String.Join(",", array)}]");
+
+static int[] InputArray()
+{
+    int[] arr = new int [8];
+    for(int i = 0; i < 8; i++)
+    {
+        arr[i] = new Random().Next(0,2);
+    }
+    return arr;
+}
+
+static void PrintArray(int[] array)
+{
+    int lenght = array.Length;
+    Console.Write("[");
+    for(int i = 0; i < lenght-1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine($"{array[lenght-1]}]");
+}
