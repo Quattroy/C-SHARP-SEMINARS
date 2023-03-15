adfb6e72c8189bf9f75ca82f8462541f522a79b1
Igor Isupov <igor-i.s1@ya.ru>
2023-03-15 21:51:44 +0300

    семинар 6

---
+++ 45/Program.cs
@@ -0,0 +1,46 @@
+﻿//Задача 45: Напишите программу, которая будет создавать 
+//копию заданного массива с помощью поэлементного копирования.
+
+int lenght = GetUserNumber("Введите длину массива ", "Некорректное значение!");
+int minNum = GetUserNumber("Введите минимальное значение элемента массива ", "Некорректное значение!");
+int maxNum = GetUserNumber("Введите максимальное значение элемента массива ", "Некорректное значение!");
+
+int[] array = GetArray(lenght, minNum, maxNum);
+
+int[] arrayCopy = GetCopy(array);
+
+Console.WriteLine($"Копия массива {String.Join(", ", array)} равна {String.Join(", ", arrayCopy)}");
+
+
+int GetUserNumber(string message, string error)
+{
+    while(true)
+    {
+        Console.WriteLine(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int num);
+        if(isCorrect && num > 0)
+            return num;
+        Console.WriteLine(error);
+
+    }
+}
+
+int[] GetArray(int leng, int min, int max)
+{
+    int[] arr = new int[leng];
+    for(int i = 0; i < leng; i++)
+    {
+        arr[i] = new Random().Next(min, max);
+    }
+    return arr;
+}
+
+int[] GetCopy(int[] arr)
+{
+    int[] copy = new int[arr.Length];
+    for(int i = 0; i < arr.Length; i++)
+    {
+        copy[i] = arr[i];
+    }
+    return copy;
+}
