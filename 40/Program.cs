cbbcdbb8498ef9972f7431b5a11f50548a6349ed
Igor Isupov <igor-i.s1@ya.ru>
2023-03-15 19:57:06 +0300

    Фибоначчи

---
+++ 40/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, 
+//может ли существовать треугольник с сторонами такой длины.
+//Теорема о неравенстве треугольника: каждая сторона треугольника меньше 
+//суммы двух других сторон
+int num1 = GetNum("Введите положительное значение A ", "Ошибка ввода!");
+int num2 = GetNum("Введите положительное значение B ", "Ошибка ввода!");
+int num3 = GetNum("Введите положительное значение C ", "Ошибка ввода!");
+
+bool result = GetResult(num1, num2, num3);
+string strResult = result ? "Существует" : "Не существует";
+Console.WriteLine($"Треугольник со сторонами {num1} {num2} {num3} -> {strResult}");
+
+
+
+int GetNum(string message, string error)
+{
+    int num = 0;
+    while (true)
+    {
+        try
+        {
+            Console.Write(message);
+            num = int.Parse(Console.ReadLine() ?? "");
+            if (num > 0) break;
+            else Console.Write(error);
+        }
+        catch (Exception exc)
+        {
+            Console.Write($"{error} {exc.Message}");
+        }
+    }
+    return num;
+}
+
+bool GetResult(int A, int B, int C)
+{
+    if(A<(B+C) && B<(A+C) && C<(A+B)) return true;
+    else return false;
+    
+}
+
---
+++ 42/Program.cs
@@ -0,0 +1,68 @@
+﻿//Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+//45 -> 101101
+//3  -> 11
+//2  -> 10
+Console.Clear();
+int userNumber = GetNumberFromUser("Введите целое положительное число: ", "Ошибка ввода!");
+int res = DiffResult1(userNumber);
+string result = DiffResult2(userNumber);
+int[] array = DiffResult3(userNumber);
+Console.WriteLine($"Число {userNumber} в двоичной системе = {result}");
+Console.WriteLine($"Число {userNumber} в двоичной системе = {res}");
+Console.WriteLine($"Число {userNumber} в двоичной системе = {String.Join("", array)}");
+
+int[] DiffResult3(int num)
+{
+    int size = 0;
+    int number = num;
+    while (number > 0)
+    {
+        number /= 2;
+        size++;
+    }
+    int[] arr = new int[size];
+    int i = size - 1;
+    while (num > 0)
+    {
+        arr[i] = num % 2;
+        i--;
+        num /= 2;
+    }
+    return arr;
+}
+string DiffResult2(int num)
+{
+    string result = "";
+    while (num > 0)
+    {
+        result = (num % 2) + result;
+        num /= 2;
+    }
+    return result;
+}
+int DiffResult1(int num)
+{
+    int result = 0;
+    int pow = 1;
+    while (num > 0)
+    {
+        result += (num % 2) * pow;
+        num /= 2;
+        pow *= 10;
+    }
+    return result;
+}
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect && userNumber > 0)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+
+
