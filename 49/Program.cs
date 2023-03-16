a5b8a4423849be4131de3d1257b0db1325185f06
Igor Isupov <igor-i.s1@ya.ru>
2023-03-16 23:31:32 +0300

    Семинар 7

---
+++ 49/Program.cs
@@ -0,0 +1,54 @@
+﻿// Решение в группах задач:
+// Задача 49: Задайте двумерный массив. Найдите элементы, 
+// у которых оба индекса нечётные, и замените эти элементы на их квадраты.
+
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+SquarOddIndexes(array);
+Console.WriteLine();
+PrintArray(array);
+
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SquarOddIndexes(int[,] arr)
+{
+    for (int i = 1; i < arr.GetLength(0); i+=2)
+    {
+        for (int j = 1; j < arr.GetLength(1); j+=2)
+        {
+            arr[i, j] = arr[i, j] * arr[i, j];
+        }
+    }
+}
---
+++ 51/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, 
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+int sumElement = SumElementIndex(array);
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumElementIndex(int[,] arr)
+{
+    int result = 0;
+    int min = arr.GetLength(0);
+    int[] num = new int[min];
+    if(min > arr.GetLength(1)) 
+    {
+        min = arr.GetLength(1);
+    }
+
+    for(int i = 0; i < min; i++)
+    {
+        result += arr[i, i];
+        num[i] = arr[i, i];
+    }
+    Console.WriteLine($"Сумма элементов на главное диагонали равна {String.Join(", ", num)} = {result}");
+    return result;
+    
+
+}
